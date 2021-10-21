bb9e586f1b0d57af3ac4339cd1c3ced9db9d19a7
ROBSON.JUNIOR <robson.ga.junior@gmail.com>
2021-10-21 13:08:45 -0300

    chore: struct

--- stalker_tools/stalker_tools.py
+++ stalker_tools/stalker_tools.py
@@ -5,5 +5,15 @@ A Stalker tool
 '''
 
 class StalkerTools():
-    def __init__(self):
-        return self
+    '''
+    StalkerTools class to facilitate api manipulation
+    
+	Parameters
+    ----------
+    domain_list : list
+        the string contains the password value.
+    '''
+
+	def __init__(self, domain_list):
+        # import pdb; pdb.set_trace()
+		return self
